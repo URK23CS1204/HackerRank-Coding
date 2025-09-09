270a88001c5db23a19de828ab19b3a9a86447889
URK23CS1204 <153933815+URK23CS1204@users.noreply.github.com>
2025-09-09 21:35:42 +0530

    Create EvenOdd.java

---
+++ Day2/EvenOdd.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int a;
+        Scanner sc = new Scanner(System.in);
+        a=sc.nextInt();
+        if(a%2==0){
+            System.out.println("EVEN");
+        }
+        else{
+            System.out.println("ODD");
+        }
+    }
+}
