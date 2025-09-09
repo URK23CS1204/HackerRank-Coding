4ea4e34ad6be4e9a8380b837b40799e86d9fe4ce
URK23CS1204 <153933815+URK23CS1204@users.noreply.github.com>
2025-09-09 21:34:55 +0530

    Create CheckEquality.java

---
+++ Day2/CheckEquality.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int a,b;
+        Scanner sc = new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        if(a==b){
+            System.out.println("EQUAL");
+        }
+        else{
+            System.out.println("NOT EQUAL");
+        }
+    }
+}
