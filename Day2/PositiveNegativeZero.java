da71bd362192771bc5e50726d32d9f25c1e6b291
URK23CS1204 <153933815+URK23CS1204@users.noreply.github.com>
2025-09-09 21:36:45 +0530

    Create PositiveNegativeZero.java

---
+++ Day2/PositiveNegativeZero.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int a;
+        Scanner sc = new Scanner(System.in);
+        a=sc.nextInt();
+        if(a>0){
+            System.out.println("Positive");
+        }
+        else if(a<0){
+            System.out.println("Negative");
+        }
+        else{
+            System.out.println("Zero");
+        }
+    }
+}
