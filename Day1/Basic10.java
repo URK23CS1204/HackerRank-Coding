8d247d7d2c8c22c7931847c956227b18fbc81030
URK23CS1204 <153933815+URK23CS1204@users.noreply.github.com>
2025-09-09 21:29:28 +0530

    Create Basic10.java

---
+++ Day1/Basic10.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        System.out.printf("3.142857142857");
+    }
+}
