cd6f169e97018ccecb143a07742a328104a65e97
URK23CS1204 <153933815+URK23CS1204@users.noreply.github.com>
2025-09-10 14:44:40 +0530

    Create Even.java

---
+++ Day3/Even.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i,start,end;
+        start=sc.nextInt();
+        end=sc.nextInt();
+        for(i=start;i<=end;i++){
+            if(i%2==0)
+                System.out.print(i+" ");
+        }
+    }
+}
