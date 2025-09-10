8491e6ff3dd7072db3835a1d2a032fc387236ec4
URK23CS1204 <153933815+URK23CS1204@users.noreply.github.com>
2025-09-10 14:43:20 +0530

    Create SumofNaturalNumbers.java

---
+++ Day3/SumofNaturalNumbers.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i,n;
+        n=sc.nextInt();
+        for(i=1;i<=n;i++)
+        {
+            
+            System.out.print(i + " ");
+        }
+        
+    }
+}
