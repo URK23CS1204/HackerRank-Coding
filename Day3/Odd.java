e085ed89dd3087c8125e1bb8f261fd486f1f70ac
URK23CS1204 <153933815+URK23CS1204@users.noreply.github.com>
2025-09-10 14:49:50 +0530

    Create Odd.java

---
+++ Day3/Odd.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n,end;
+        end=sc.nextInt();
+        for(int i=1;i<=end;i++){
+            if(i%2!=0)
+                System.out.print(i + " ");
+        }
+    }
+}
